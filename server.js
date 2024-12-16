030c85ff7525bb8d5e0fc24e67ee942225953f91
Sina <sinasalah6@gmail.com>
2024-12-16 15:39:32 +0300

    Fix MongoDB connection warnings and clean up server.js

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ app.use(express.json()); // For parsing application/json
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
